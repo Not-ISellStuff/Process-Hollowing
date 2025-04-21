f2d713288ad9e3f9c027c210a4708b9f9823b8e5
ISellStuff <174392963+Not-ISellStuff@users.noreply.github.com>
2025-04-21 11:29:26 -0700

    Update loader.h

--- loader.h
+++ loader.h
@@ -2,6 +2,11 @@
 //  Shell Code Injector //
 /////////////////////////
 
+#ifndef LOADER_H
+#define LOADER_H
+
 #include <windows.h>
 
-int load(unsigned char* shellcode, DWORD pid);
+int load(unsigned char* shellcode, size_t size, DWORD pid);
+
+#endif
